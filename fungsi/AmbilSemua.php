b5f3781ff6e20af175ac362bf5e6d7c01a9ac305
anwar saifulloh <anwar.saifulloh@gmail.com>
2013-04-23 06:00:19 +0700

    fungsi ambil artikkel rss

---
+++ fungsi/AmbilSemua.php
@@ -0,0 +1,19 @@
+<?php
+function parser($feedURL) {
+	$rss = simplexml_load_file($feedURL);
+	echo "<ul class='news'>";
+	$i = 0;
+	foreach ($rss->channel->item as $feedItem) {
+		$i++;
+		$myDate = ($feedItem->pubDate);
+		$dateForm = explode(" ", $myDate);
+		echo "<li>
+			<h2 class='news'><a href='$feedItem->link' title='$feedItem->title'>" . $feedItem->title . "</a></h2>
+			<p class='desc'>" . $feedItem->description . "</p>
+			<p class='date'>Posted on: " . $dateForm[1] . ". " . $dateForm[2] . ". " . $dateForm[3] . "." . "<a class='cont' href='$feedItem->link' title='$feedItem->title'>Continue Reading</a></p>
+		</li>";
+		if($i >= 5) break;
+	}
+	echo "</ul>";
+}
+?>
